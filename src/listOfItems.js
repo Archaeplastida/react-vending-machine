4369425634fa9c8715bb8b0fd5e2b4d576297508
Saif <saifisami2005@gmail.com>
2024-12-17 16:16:21 -0500

    Added list of items for the vending machine.

---
+++ src/listOfItems.js
@@ -0,0 +1,22 @@
+const listOfItems = [
+    {
+        name: "water",
+        displayName: "Water",
+        description: "Hydrating fluid, essential for everyone!",
+        imgURL: "https://beverages2u.com/wp-content/uploads/2019/05/nestlebottle-2.png"
+    },
+    {
+        name: "pepsi",
+        displayName: "Pepsi",
+        description: "Energizing, while being thirst-quenching!",
+        imgURL: "https://static.vecteezy.com/system/resources/previews/036/576/233/non_2x/bottle-of-pepsi-cola-isolated-free-png.png"
+    },
+    {
+        name: "chips",
+        displayName: "Chips",
+        description: "Delicious flavors to crunch on the go!",
+        imgURL: "https://png.pngtree.com/png-clipart/20230914/original/pngtree-bag-of-chips-vector-png-image_12148306.png"
+    }
+]
+
+export default listOfItems;
