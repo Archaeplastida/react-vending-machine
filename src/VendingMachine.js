041da6367689e65340610554eb382348760b5c7f
Saif <saifisami2005@gmail.com>
2024-12-17 17:00:57 -0500

    Added slight styling changes and one more item.

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -5,12 +5,14 @@ import itemsList from "./listOfItems";
 function VendingMachine() {
     return (
         <div>
+            <h1>Vending Machine</h1>
+            <img style={{height: "200px"}} src="https://www.betson.com/wp-content/uploads/2019/07/evoke-6-vending-machine.png"></img>
             <div className="vending-machine-message">
                 <p>What do you want?</p>
             </div>
-            <div className="vending-machine-items">
-                {itemsList.map(item => <Link to={item.name}>{item.displayName}</Link>)}
-            </div>
+            <ul className="vending-machine-items">
+                {itemsList.map(item => <li><Link to={item.name}>{item.displayName}</Link></li>)}
+            </ul>
         </div>
     )
 }
--- src/listOfItems.js
+++ src/listOfItems.js
@@ -16,6 +16,12 @@ const listOfItems = [
         displayName: "Chips",
         description: "Delicious flavors to crunch on the go!",
         imgURL: "https://png.pngtree.com/png-clipart/20230914/original/pngtree-bag-of-chips-vector-png-image_12148306.png"
+    },
+    {
+        name: "spicy-chips",
+        displayName: "Spicy Chips",
+        description: "Take some spice on top of your crunch on the go!",
+        imgURL: "https://shop.herrs.com/cdn/shop/products/0004_Red-Hot-Chips_2048x.png"
     }
 ]
 
