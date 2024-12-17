2a05425d7839c4fa2eba7ba7f8cb43b6e20ff7e1
Saif <saifisami2005@gmail.com>
2024-12-17 16:15:53 -0500

    Finished implementation.

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import itemsList from "./listOfItems";
 
 function VendingMachine() {
     return (
-        <div></div>
+        <div>
+            <div className="vending-machine-message">
+                <p>What do you want?</p>
+            </div>
+            <div className="vending-machine-items">
+                {itemsList.map(item => <Link to={item.name}>{item.displayName}</Link>)}
+            </div>
+        </div>
     )
 }
 
